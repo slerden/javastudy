272c2abd567bdb097e8e925d6f71916db92c2a31
Oleg Flegontov <slerden44@gmail.com>
2016-12-22 15:34:46 +0300

    first commit

---
+++ src/main/java/ru/wa/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package ru.wa.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Oleg on 22.12.2016.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main(){
+        return new ModelAndView("index");
+    }
+}
